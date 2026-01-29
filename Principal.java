bb3f0b709c99cf82e9a23decf7064899b6bf186a
Moipr99 <moipr9@gmail.com>
2026-01-29 14:09:21 +0100

    Implement method to get current date and time
    Added method to obtain current date and time.

--- Principal.java
+++ Principal.java
@@ -1,7 +1,18 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola, mundo");
 
         System.out.println("Adios mundo");
     }
+
+    // Método fecha y hora actual
+    public static String obtenerFechaHora() {
+        LocalDateTime fecha = LocalDateTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        return fecha.format(formato);
+    }
+    
 }
